87032cfc33019a2ae694ae62955445a06e6ba7ba
Shir-Av <57191216+Shir-Av@users.noreply.github.com>
2020-11-03 16:45:54 +0200

    init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Tooltip("This is the object that the camera will follow")]
+    [SerializeField]
+    GameObject toFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(toFollow.gameObject.transform.position.x, toFollow.gameObject.transform.position.y + 3, transform.position.z);
+
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [Tooltip("Spaceship Speed")]
+    [SerializeField]
+    float speed = 5f;
+
+   /* public GameObject topBorder;
+    public GameObject bottomBorder;
+    public GameObject leftBorder;
+    public GameObject rightBorder;*/
+
+    private Quaternion startRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = startRotation;
+        if (transform.position.z != 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
